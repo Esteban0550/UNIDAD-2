f2d7f4a7e7217a227f426d9bcf42fc09e0cd6ff8
Esteban0550 <estebanpriego2005@gmail.com>
2025-03-05 13:26:50 -0600

    PriceTag Added

--- donut_app_2c_priego/lib/tabs/donut_tab.dart
+++ donut_app_2c_priego/lib/tabs/donut_tab.dart
@@ -1,44 +1,41 @@
+import 'package:donut_app_2c/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
  
 class DonutTab extends StatelessWidget {
-
-  //lista de donas
-    final List donutsOnSale = [
-
-   // [ donutFlavor, donutPrice, donutColor, imageName ]
-
-   ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-   ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-   ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-   ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-
-];
-
-   DonutTab({super.key});
+  //Lista de donas
+  final List donutsOnSale = [
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
+  DonutTab({super.key});
  
   @override
   Widget build(BuildContext context) {
-    //widget para cuadricula 
+    //Widget para usar cuadrícula
     return GridView.builder(
-      itemCount: donutsOnSale.length,
-      padding: EdgeInsets.all(12),
-      //Organiza como se veran los elemtos
-      gridDelegate: 
-        SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, //columnas
-         
-  
-
-
-      ),
-     itemBuilder: (context, index) {
-        return DonutTile(
-          donutFlavor: donutsOnSale[index][0],
-          donutPrice: donutsOnSale[index][1],
-          donutColor: donutsOnSale[index][2],
-          imageName: donutsOnSale[index][3],
-        );
-      },
-    );
+        //Cuantos elementos hay en la cuadricula
+        itemCount: donutsOnSale.length,
+        padding: EdgeInsets.all(12),
+        //o
+        //Cómo se distribuiran los elementos
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //Cuantas colummnas
+          crossAxisCount: 2,
+          //Relacion aspecto (Proporcion)
+          childAspectRatio: 1 / 1.5,
+        ),
+        itemBuilder: (context, index) {
+          //Cada elemento indivual
+          return DonutTile(
+            donutFlavor: donutsOnSale[index][0],
+            donutPrice: donutsOnSale[index][1],
+            donutColor: donutsOnSale[index][2],
+            imageName: donutsOnSale[index][3],
+          );
+        });
   }
 }
+ 
