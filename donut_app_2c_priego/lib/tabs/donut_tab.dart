e08a63ce2c00e2e8c133b1416c49136b01302d7b
Esteban0550 <estebanpriego2005@gmail.com>
2025-03-07 13:16:59 -0600

    DonutTile Finished

--- donut_app_2c_priego/lib/tabs/donut_tab.dart
+++ donut_app_2c_priego/lib/tabs/donut_tab.dart
@@ -1,41 +1,43 @@
 import 'package:donut_app_2c/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
- 
+
 class DonutTab extends StatelessWidget {
-  //Lista de donas
+  // Lista de donas
   final List donutsOnSale = [
     // [ donutFlavor, donutPrice, donutColor, imageName ]
-    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    ["Ice Cream", "Krispy Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "Dunkin Donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "Ahorrera", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "PriegoStore", "95", Colors.brown, "lib/images/chocolate_donut.png"],
   ];
+
   DonutTab({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
-    //Widget para usar cuadrícula
+    // Widget para usar cuadrícula
     return GridView.builder(
-        //Cuantos elementos hay en la cuadricula
-        itemCount: donutsOnSale.length,
-        padding: EdgeInsets.all(12),
-        //o
-        //Cómo se distribuiran los elementos
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          //Cuantas colummnas
-          crossAxisCount: 2,
-          //Relacion aspecto (Proporcion)
-          childAspectRatio: 1 / 1.5,
-        ),
-        itemBuilder: (context, index) {
-          //Cada elemento indivual
-          return DonutTile(
-            donutFlavor: donutsOnSale[index][0],
-            donutPrice: donutsOnSale[index][1],
-            donutColor: donutsOnSale[index][2],
-            imageName: donutsOnSale[index][3],
-          );
-        });
+      // Cuantos elementos hay en la cuadrícula
+      itemCount: donutsOnSale.length,
+      padding: EdgeInsets.all(12),
+      // o
+      // Cómo se distribuirán los elementos
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        // Cuantas columnas
+        crossAxisCount: 2,
+        // Relación aspecto (Proporción)
+        childAspectRatio: 1 / 1.6,
+      ),
+      itemBuilder: (context, index) {
+        // Cada elemento individual
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutBrand: donutsOnSale[index][1],
+          donutPrice: donutsOnSale[index][2],
+          donutColor: donutsOnSale[index][3],
+          imageName: donutsOnSale[index][4],
+        );
+      },
+    );
   }
 }
- 
--- donut_app_2c_priego/lib/utils/donut_tile.dart
+++ donut_app_2c_priego/lib/utils/donut_tile.dart
@@ -1,71 +1,115 @@
 import 'package:flutter/material.dart';
- 
+
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
   final String donutPrice;
+  final String donutBrand;
   // Es dinámico porque es de tipo color
   final dynamic donutColor;
   final String imageName;
- 
+
   const DonutTile({
     super.key,
     required this.donutFlavor,
     required this.donutPrice,
+    required this.donutBrand,
     this.donutColor,
     required this.imageName,
   });
- 
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: donutColor[50],
-          borderRadius: BorderRadius.circular(24.0),
-        ),
-        child: Column(
-          children: [
-            // price tag
-            Row(
-              // Alinea a la derecha
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: donutColor[100],
-                    borderRadius: BorderRadius.only(
+      child: SingleChildScrollView(  // Envuelve el Column en SingleChildScrollView
+        child: Container(
+          decoration: BoxDecoration(
+            color: donutColor[50],
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+          child: Column(
+            children: [
+              // price tag
+              Row(
+                // Alinea a la derecha
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: donutColor[100],
+                      borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(24),
-                        topRight: Radius.circular(24)),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 18,
-                    vertical: 8,
-                  ),
-                  child: Text(
-                    '\$$donutPrice',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: donutColor[800],
+                        topRight: Radius.circular(24),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 18,
+                      vertical: 8,
+                    ),
+                    child: Text(
+                      '\$$donutPrice',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: donutColor[800],
+                      ),
                     ),
                   ),
+                ],
+              ),
+              // Donut Picture
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                child: Image.asset(
+                  imageName,
                 ),
-              ],
-            ),
-            // Donut Picture
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal:24),
-              child: Image.asset(imageName,),
-            )
- 
-            // Donut Flavor text
- 
-            // Botones
-          ],
+              ),
+              // Donut Flavor text
+              Text(
+                donutFlavor,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Text(
+                donutBrand,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+              ),
+              // Espacio entre textos
+              SizedBox(
+                height: 4,
+              ),
+  
+              // Botones
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Botón de "favorito"
+                  IconButton(
+                    icon: Icon(
+                    
+                  
+                      Icons.favorite_border,
+                      color: Colors.pink[400], // Color Pink 400
+                    ),
+                    onPressed: () {
+                      // Acción para el botón de favorito
+                    },
+                  ),
+                  // Botón de "agregar"
+                  IconButton(
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.grey[800], // Color Grey 800
+                    ),
+                    onPressed: () {
+                      // Acción para el botón de agregar
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
- 
